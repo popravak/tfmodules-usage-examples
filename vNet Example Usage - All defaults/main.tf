59424caadbd81d4179c4950a0061b276fa9782be
sasa <sasa.popravak@gmail.com>
2022-11-01 11:56:51 +0100

    vNet examples w/ and w/o defaults

---
+++ vNet Example Usage - All defaults/main.tf
@@ -0,0 +1,30 @@
+
+module "rg" {
+  source  = "popravak/rg/azurerm"
+  version = "1.0.1"
+  //rg_name = "rgspo001"
+}
+
+module "vnet" {
+  source  = "popravak/vnet/azurerm"
+  version = "1.0.0"
+  //business_unit_prefix = "finance"
+  //environment_prefix   = "dev"
+  rg_name = module.rg.rg_name
+  /*
+  location              = "northeurope"
+  vnet_address_space    = ["10.0.0.0/16", "192.168.0.0/16"]
+  subnets = { 
+    "front"   = { subnet = ["10.0.0.0/24"] },
+    "mid"     = { subnet = ["10.0.1.0/24"] },
+    "back"    = { subnet = ["10.0.2.0/24"] },
+    "bastion" = { subnet = ["192.168.0.0/24"] } 
+  }
+  tags = { 
+    "unit" = "IT",
+    "env"   = "dev",
+    "owner" = "Sasa Popravak"
+  }
+*/
+}
+
